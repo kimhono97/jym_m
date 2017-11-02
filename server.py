371576c1c52ba8ee1356f1b3fb337f2d1d84d0cb
kimhono97 <kimhono97@gmail.com>
2017-11-02 16:55:32 +0900

    Fixed Pull Error

--- server.py
+++ server.py
@@ -1,33 +1,14 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 from crawling_qt import getQT
 from crawling_notice import getNotice
-from FIO_qt import writeQTpage
-import time
+from crawling_instagup import get_instagup
 
 app = Flask(__name__)
-
-def getTime():
-    t = time.strftime("%x", time.localtime(time.time())).split("/")
-    t = t[2] + t[0] + t[1]
-    return t
 
 # ============================================================================
 
 @app.route("/qt")
 def qt():
-    now = int(getTime())
-    f = open("qt_date.txt", "r")
-    date = int(f.readline())
-    f.close()
-#    if now != date:
-#        all_qt = {}
-#        for lang in ['kr', 'en', 'cn', 'jp']:
-#            all_qt[lang] = getQT(lang)
-#        writeQTpage(now, render_template("qt_page.html", all_qt=all_qt))
-#        f = open("qt_date.txt", "w")
-#        f.write(now)
-#        f.close()
-#    return render_template("qt_page_done.html")
     all_qt = []
     for lang in ['kr', 'en', 'cn', 'jp']:
         all_qt.append( getQT(lang) )
@@ -58,14 +39,14 @@ def time_table_demo():
 def root():
     lang = "kr"
     qt = getQT(lang)
+    insta = get_instagup()
     notice = getNotice(3)
-    return render_template("index.html", qt=qt, notice=notice)
+    return render_template("index.html", qt=qt, notice=notice, insta=insta)
 
 # =============================================================================
 
 if __name__ == "__main__":
-    f = open("qt_date.txt", "w")
-    f.write("0")
-    f.close()
     app.debug = True
-    app.run(host="192.168.0.138", port=80, threaded=True)
+    app.run(host="172.31.44.210", threaded=True)
+    # AWS   : 172.31.44.210:5000
+    # JYM_c : 175.193.125.177:80
